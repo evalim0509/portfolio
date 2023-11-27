37cb3fd9101a3da2c3c95a97fde581fd07b80113
evalim0509 <152061252+evalim0509@users.noreply.github.com>
2023-11-27 12:26:34 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,8 @@
-/**
-* Template Name: DevFolio
-* Updated: Nov 17 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+<script>
+// Get the video
+var video = document.getElementById("myVideo");
+</script>
+  
 (function() {
   "use strict";
 
